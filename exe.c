41395f59c63cfd87ec2b810dad28456a4f15abf3
LiciaMor18 <124669136+LiciaMor18@users.noreply.github.com>
2023-02-08 16:19:47 +0100

    Add files via upload

---
+++ exe.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main ()
+
+
+{
+    int x;
+    
+
+    printf ("inserisci numero:\n" );
+    scanf ("%d", &x );
+    int y = x - 1;
+
+    while (x % y != 0 && y > 1)
+    {
+        y = y - 1 ;
+    }
+        if ( y == 1  )
+        {
+            printf ("numero primo\n");
+        
+        }
+        
+        else
+        {
+            printf("numero non primo %d \n", x);
+        }
+        
+
+}
+
