021d53f333e4d5dc02903a49e12b893a1f3065ae
LiciaMor18 <124669136+LiciaMor18@users.noreply.github.com>
2023-02-09 17:03:09 +0100

    Add files via upload

---
+++ array3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//crea un array di intari riempilo di numeri a scalta utilizzando una sola variabile extra inverire l'ordine degli elementi 
+//ex array 77 2 9 11 5 3 array invertito 3 5 11 9 2
+
+int main()
+{
+    int array [5];
+    
+    
+
+    printf (" inserisci un numero: %d\n");
+    scanf ("%d" , &array );
+
+    while ( i <= 4)
+    {
+        
+        i = i + 1;
+        x = x + 2;
+
+    }
+    i = 4 ;
+
+    while ( i >= 0)
+    {
+        printf ("%d\n", array[i]); 
+        i = i - 1;
+    }
+}
+
